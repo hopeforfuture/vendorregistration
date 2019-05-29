fb72fdfa6176befc8b6f48f660d7ffb19ad95239
nilanjan <nilanjan.b@expressgrp.com>
2019-05-29 15:45:19 +0530

    check git

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller 
-{
+{       private $name;
 	public function __construct()
 	{
 		parent::__construct();

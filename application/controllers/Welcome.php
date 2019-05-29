de0ee4097bdd8f19e2817ce5be6af6a3adbeb13d
hopeforfuture <mnbl87@gmail.com>
2019-05-29 17:03:21 +0530

    Commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller 
-{       private $name;
+{  
 	public function __construct()
 	{
 		parent::__construct();
@@ -25,7 +25,6 @@ class Welcome extends CI_Controller
 	 */
 	public function index()
 	{
-		$data = array();
 		$this->load->view('user/index');
 	}
 }

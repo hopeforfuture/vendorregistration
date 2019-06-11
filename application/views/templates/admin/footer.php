44a0937db06fc3ef5eaaf018c127bff306d036e7
Manojit Nandi <mnbl87@gmail.com>
2019-06-11 15:11:28 +0530

    commit changes

--- application/views/templates/admin/footer.php
+++ application/views/templates/admin/footer.php
@@ -1,33 +1,9 @@
 </div>
-        <!-- content-wrapper ends -->
-		
-		
-        <!-- partial:../../partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?php echo date("Y", time()); ?> <a href="Javascript:void(0)" >Farmneed</a>. All rights reserved.</span>
-            <!--<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>-->
-          </div>
-        </footer>
-        <!-- partial -->
-		
-		
-      </div>
-      <!-- main-panel ends -->
-	  
-    </div>
-    <!-- page-body-wrapper ends -->
-	
-  </div>
-  <!-- container-scroller -->
-  
-  <!-- plugins:js -->
-  <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/jquery-ui.js"></script>
-  <script src="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/template.js"></script>
+<!-- End of animated content -->
+</div>
+<!-- End of content  -->
+</div>
+<!-- End of Right Panel  -->
 </body>
 
 </html>
--- application/views/templates/admin/header.php
+++ application/views/templates/admin/header.php
@@ -1,153 +1,111 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Administrator Panel</title>
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styleadmin.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery-ui.css">
-  <!--<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">-->
-  
-  <!-- endinject -->
-  <!--<link rel="shortcut icon" href="../../images/favicon.png" />-->
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Farm Need - Vendor Registration</title>
+    <meta name="description" content="Fram need admin">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/logo.png">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+    <link href="<?php echo base_url(); ?>assets/css/fullcalendar.min.css" rel="stylesheet" />
+    <style>
+	#weatherWidget .currentDesc {
+		color: #ffffff!important;
+	}
+	.traffic-chart {
+		min-height: 335px;
+	}
+	#flotPie1 {
+		height: 150px;
+	}
+	#flotPie1 td {
+		padding: 3px;
+	}
+	#flotPie1 table {
+		top: 20px!important;
+		right: -10px!important;
+	}
+	.chart-container {
+		display: table;
+		min-width: 270px;
+		text-align: left;
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+	#flotLine5 {
+		height: 105px;
+	}
+	#flotBarChart {
+		height: 150px;
+	}
+	#cellPaiChart {
+		height: 160px;
+	}
+</style>
 </head>
 
 <body>
-  <div class="container-scroller">
-    <!-- partial:../../partials/_navbar.html -->
-    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-       <div class="navbar-brand-wrapper d-flex justify-content-center">
-        <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
-          <a  href="Javascript:void(0)">
-      <img src="<?php echo base_url(); ?>assets/icons/logo-ldpi.png" alt="logo">
-      </a>
-      
-          <!--<a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="../../images/logo-mini.svg" alt="logo"/></a>-->
-          <!--<button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-            <span class="mdi mdi-sort-variant"></span>
-          </button>-->
-        </div>  
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-        <ul class="navbar-nav mr-lg-4 w-100">
-          <li class="nav-item nav-search d-none d-lg-block w-100">
-            <div class="input-group">
-              <!--<div class="input-group-prepend">
-                <span class="input-group-text" id="search">
-                  <i class="mdi mdi-magnify"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" placeholder="Search now" aria-label="search" aria-describedby="search">-->
-            </div>
-          </li>
-        </ul>
-    
-    
-    
-        <ul class="navbar-nav navbar-nav-right">
-      
-
-          <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-              <!--<img src="../../images/faces/face5.jpg" alt="profile"/>-->
-              <span class="nav-profile-name"><?php echo $sessdata['u_name']; ?></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-
-              <!--<a class="dropdown-item">
-                <i class="mdi mdi-settings text-primary"></i>
-                Settings
-              </a>-->
-
-              <a href="<?php echo base_url('admin/logout'); ?>" class="dropdown-item">
-                <i class="mdi mdi-logout text-primary"></i>
-                Logout
-              </a>
-            </div>
-          </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="mdi mdi-menu"></span>
-        </button>
-      </div>
-
-
-    </nav>
-    <!-- partial -->
-  
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:../../partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="../../index.html">
-              <i class="mdi mdi-home menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link"  href="<?php echo base_url('admin/dashboard'); ?>">
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="mdi mdi-account menu-icon"></i>
-              <span class="menu-title">Vendors</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/list'); ?>"> List </a></li>
-              </ul>
-            </div>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="mdi mdi-account menu-icon"></i>
-              <span class="menu-title">Invoice</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist'); ?>"> All </a></li>
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist/P'); ?>"> Paid </a></li>
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist/U'); ?>"> Unpaid </a></li>
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist/AP'); ?>"> Approved </a></li>
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist/RE'); ?>"> Rejected </a></li>
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/vendor/filelist/PE'); ?>"> Pending </a></li>
-              </ul>
-            </div>
-          </li>
-		  
-		  <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="mdi mdi-account menu-icon"></i>
-              <span class="menu-title">Contact List</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/contact/list'); ?>"> List </a></li>
-				<li class="nav-item"> <a class="nav-link" href="<?php echo base_url('admin/contact/add'); ?>"> Add </a></li>
-              </ul>
-            </div>
-          </li>
-
-        </ul>
-      </nav>
-      
-      <!-- partial -->
-      <div class="main-panel">
-        <div class="content-wrapper">
+	<!-- Left Panel -->
+	<aside id="left-panel" class="left-panel">
+		  <nav class="navbar navbar-expand-sm navbar-default">
+				<div id="main-menu" class="main-menu collapse navbar-collapse">
+				  <ul class="nav navbar-nav">
+					<li class="active"> <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a> </li>
+					<li> <a href="#"><i class="menu-icon fa fa-laptop"></i> Vendor </a> </li>
+				  </ul>
+				</div>
+		<!-- /.navbar-collapse --> 
+		 </nav>
+	</aside>
+	<!-- /#left-panel --> 
+	
+	<div id="right-panel" class="right-panel">
+	
+		<!-- Header-->
+		<header id="header" class="header">
+			  <div class="top-left">
+				  <div class="navbar-header"> 
+					  <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a> 
+					  <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a> 
+					  <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a> 
+				  </div>
+			  </div>
+			  <div class="top-right">
+				<div class="header-menu">
+					  <div class="header-left">
+							<button class="search-trigger"><i class="fa fa-search"></i></button>
+							<div class="form-inline">
+								<form class="search-form">
+									<input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
+									<button class="search-close" type="submit"><i class="fa fa-close"></i></button>
+								</form>
+							</div>
+					   </div>
+					   
+					  <div class="user-area dropdown float-right"> 
+						  <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 	<img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar"> 
+						  </a>
+						  <div class="user-menu dropdown-menu"> 
+							<a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a> 
+							<a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a> 
+							<a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a> 
+							<a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a> 
+						  </div>
+				      </div>
+					   
+			    </div>
+		     </div>
+      </header>
+	 <!-- /#header --> 
+	 
+	 <!-- Content -->
+	 <div class="content">
+		<!-- Animated -->
+       <div class="animated fadeIn"> 
